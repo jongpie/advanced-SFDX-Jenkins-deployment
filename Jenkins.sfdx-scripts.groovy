2b1ee7a1b29e2a4785fbd485cb8c93627e9bb0d9
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2021-04-23 14:20:03 -0700

    sdf32

--- Jenkins.sfdx-scripts.groovy
+++ Jenkins.sfdx-scripts.groovy
@@ -175,7 +175,7 @@ def upsertCsvFiles(salesforceEnvironment) {//}, sobjectType, externalId) {
     def environmentDetails = loadSfdxEnvironment(salesforceEnvironment)
     for(csvFile in environmentDetails.csvDataToUpsert) {
         echo 'Upserting ' + csvFile.filename + ' for SObject Type ' + csvFile.sobjectType + ' using external ID field ' + csvFile.externalIdField
-        def upsertCommand = 'sfdx force:data:bulk:upsert --sobjecttype ' + csvFile.sobjectType + ' --externalid ' + csvFile.externalIdField + ' --csvfile ' + csvFile.filename + ' --targetusername ' + salesforceEnvironment
+        def upsertCommand = 'sfdx force:data:bulk:upsert --json --sobjecttype ' + csvFile.sobjectType + ' --externalid ' + csvFile.externalIdField + ' --csvfile ' + csvFile.filename + ' --targetusername ' + salesforceEnvironment
         echo 'upsertCommand==' + upsertCommand
         runCommand(upsertCommand)
     }
